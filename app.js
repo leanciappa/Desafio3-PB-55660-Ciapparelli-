3e71d99c9dd1ad46c6b63e097cd78836dcff1eb0
leanciappa <ciapparelli.leandro@gmail.com>
2024-01-10 23:52:58 -0300

    V1.0

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require ("express");
+const app = express ();
+
+app.get ("/ping", (req, res)=>{
+    res.send("pong")
+})
+
+app.listen (3000, ()=> {
+    console.log ("Aplicación funcionando en el puerto 3000")
+
+})
